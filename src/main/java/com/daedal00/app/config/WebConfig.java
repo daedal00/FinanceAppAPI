2eb6a885db089b714d4b194384e25f9640a2b8f1
daedal00 <92826197+daedal00@users.noreply.github.com>
2023-10-09 12:12:50 -0700

    disabled csrf

--- src/main/java/com/daedal00/app/config/WebConfig.java
+++ src/main/java/com/daedal00/app/config/WebConfig.java
@@ -1,16 +1,19 @@
-// package com.daedal00.app.config;
+package com.daedal00.app.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// @Configuration
-// @EnableWebMvc
-// public class WebConfig implements WebMvcConfigurer {
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
 
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**");
-//     }
-// }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Specify the exact origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true); // Allow sending cookies
+    }
+}
--- src/main/java/com/daedal00/app/config/WebSecurityConfig.java
+++ src/main/java/com/daedal00/app/config/WebSecurityConfig.java
@@ -13,13 +13,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.daedal00.app.util.JwtRequestFilter;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -27,28 +22,10 @@ public class WebSecurityConfig {
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // Allow frontend origin
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowCredentials(true); // Important for sending and receiving cookies
-        configuration.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization", "X-XSRF-TOKEN"));
-        configuration.setExposedHeaders(Arrays.asList("X-XSRF-TOKEN")); // Expose the CSRF token header
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> 
-                csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            )
+            .csrf(csrf -> csrf.disable()) // Disable CSRF protection
             .authorizeHttpRequests(authorizeRequests ->
                 authorizeRequests
                     .requestMatchers("/api/users/register", "/api/users/login").permitAll()
@@ -64,3 +41,4 @@ public class WebSecurityConfig {
         return new ProviderManager(Collections.singletonList(authenticationProvider));
     }
 }
+
